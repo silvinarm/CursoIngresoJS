3dd78338a91e3afe8dfc3d0e7527390a5a08564a
silvinarm <silvinarm@hotmail.com>
2016-11-14 19:09:27 -0300

    Primer_ejercicio

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,20 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+ej.: "Usted se llama José y tiene 66 años" 	
+
 function Mostar()
-{	
-	
+{
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert (nombre);
+}
+*/
+function Mostar()
+{	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	var edad;
+	edad = document.getElementById('laEdad').value;
+	alert ("Usted se llama "+nombre+ " y tiene "+edad+" años");
+
+		
 }
 
