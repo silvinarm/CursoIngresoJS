f705a1d3fea6a74c017cb0260a2c08df7227ca2e
silvinarm <silvinarm@hotmail.com>
2016-11-14 19:01:20 -0300

    Primer_ejercicio

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,28 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+
+/*para definir una variable pongo var el_nombre ;
+luego defino el_nombre = "xxx"
+
+EJ
+
 function Mostar()
 {
-	
+	var producto ;
+	producto = "samsung";
+	alert (producto);
+}
+si lo que inserto es un valor variable va a mostrar ese valor
+
+para concatenar usamos el + -> Ejemplo alert ("su producto es: " +producto+ " el precio es " +precio);
+*/
+
+function Mostar()
+{
+	var producto ;
+	producto = prompt ("ingrese marca","sin marca");
+	var precio ;
+	precio = prompt ("ingrese precio","999");
+	alert ("su producto es: " +producto+ " el precio es " +precio);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,24 @@
 /*Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+
+/*id="elNombre">
+
+document.g tab shift i
+
 function Mostar()
 {
-	
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert (nombre);
+}
+
+*/
+
+function Mostar()
+{
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert (nombre);
 }
 
 
