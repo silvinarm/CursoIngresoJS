83f3224020a5e5bfee4127b2afb0541998d25d13
silvinarm <silvinarm@hotmail.com>
2016-11-14 18:56:26 -0300

    Commit_clase_1

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,16 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+
+"elNombre"
+
+*/
+function Mostar()
+{
+	var dat 
+	dat = prompt ("ingrese dato");
+	document.getElementById('elNombre').value=dat ;
+
+	
+}
+
