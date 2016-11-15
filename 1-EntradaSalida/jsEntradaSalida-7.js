9533a3e9c899dc1ed44b888fd6a1a57d4eb7b3d9
silvinarm <silvinarm@hotmail.com>
2016-11-15 11:31:19 -0300

    Ejercicio_resuelto

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,58 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{	var nuno;
+	var ndos;
+	var resultado;
+
+	nuno = document.getElementById('numeroUno').value;
+	nuno = parseInt (nuno);
+	ndos = document.getElementById ('numeroDos').value;
+	ndos = parseInt (ndos);
+	resultado = (nuno + ndos);
+	alert ("la suma es "+resultado);
 	
 }
 
 function restar()
-{
+{	var nuno;
+	var ndos;
+	var resultado;
+
+	nuno = document.getElementById('numeroUno').value;
+	nuno = parseInt (nuno);
+	ndos = document.getElementById ('numeroDos').value;
+	ndos = parseInt (ndos);
+	resultado = (nuno - ndos);
+	alert ("la resta es "+resultado);
 	
 }
 
 function multiplicar()
-{ 
+{	var nuno;
+	var ndos;
+	var resultado;
+
+	nuno = document.getElementById('numeroUno').value;
+	nuno = parseInt (nuno);
+	ndos = document.getElementById ('numeroDos').value;
+	ndos = parseInt (ndos);
+	resultado = (nuno * ndos);
+	alert ("la multiplicacion es "+resultado);
 	
 }
 
 function dividir()
-{
+{	var nuno;
+	var ndos;
+	var resultado;
+
+	nuno = document.getElementById('numeroUno').value;
+	nuno = parseInt (nuno);
+	ndos = document.getElementById ('numeroDos').value;
+	ndos = parseInt (ndos);
+	resultado = (nuno / ndos);
+	alert ("la division es "+resultado);
 	
 }
 
