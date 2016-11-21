bd87693622d8c283e1369e199bd4cd9226a9ab5a
silvinarm <silvinarm@hotmail.com>
2016-11-21 19:27:21 -0300

    Ejercicio resuelto

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,25 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numeroRandom;
+
+	numeroRandom = Math.floor ((Math.random () *10)+1);
+	console.log ("La nota es "+numeroRandom);
+	if (numeroRandom > 8)	
+	{
+		alert ("Excelente");
+	}
+	else
+	{
+		if (numeroRandom < 4)
+		{
+			alert ("Vamos, la próxima se puede");
+		}
+		else
+		{
+			alert ("Aprobó");
+		}
+	}
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+
+	numeroRandom = Math.floor ((Math.random () *10)+1);
+	//document.getElementById('FormIngreso').value=numeroRandom ;
+	alert (numeroRandom);
+
+
 
 }//FIN DE LA FUNCIÓN
