bb58671bf080b87c1997cdf3a26e52a4f2f4b49c
silvinarm <silvinarm@hotmail.com>
2016-11-26 11:00:32 -0300

    Ejercicio_Resuelto

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,55 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var PrimerNumero;
+var SegundoNumero;
+var Operador;
+
 function comenzar()
 {
-
+	document.getElementById ('Respuesta').value = null;
+	PrimerNumero = Math.floor ((Math.random ()*10)+1);
+ 	document.getElementById('PrimerNumero').value = PrimerNumero;	
+ 	SegundoNumero = Math.floor ((Math.random ()*10)+1);
+ 	document.getElementById ('SegundoNumero').value = SegundoNumero;
+ 	Operador = ["*","+","/","-"] [Math.floor (Math.random()*4)];
+ 	document.getElementById('Operador').value = Operador;
 	
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
+	var CalculoCorrecto;
+	var Respuesta;
+
+	
+	if (Operador == "*")
+	{
+		CalculoCorrecto = (PrimerNumero*SegundoNumero);
+	}
+	if (Operador == "/")
+	{
+		CalculoCorrecto = (PrimerNumero/SegundoNumero);
+	}
+	if (Operador == "+")
+	{
+		CalculoCorrecto = (PrimerNumero+SegundoNumero);
+	}
+	if (Operador == "-")
+	{
+		CalculoCorrecto = (PrimerNumero-SegundoNumero);
+	}
+	console.log ("El resultado correcto es "+CalculoCorrecto);
+	Respuesta = document.getElementById('Respuesta').value;
+	if (CalculoCorrecto == Respuesta)
+	{
+		alert("El resultado es correcto");
+	}
+	else
+	{
+		alert("El resultado es incorrecto");
+	}
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsAgilidadAritmetica2.js
+++ 8-TPs/jsAgilidadAritmetica2.js
@@ -4,14 +4,55 @@ se agregara un temporizador que
  el juego  de no ser ingresado el resultado 
  correcto en ese lapso de tiempo. */
 var respuesta;
+var PrimerNumero;
+var SegundoNumero;
+var Operador;
 var temporizador;
+
 function comenzar()
 {
-	
+	document.getElementById ('Respuesta').value = null;
+	PrimerNumero = Math.floor ((Math.random ()*10)+1);
+ 	document.getElementById('PrimerNumero').value = PrimerNumero;	
+ 	SegundoNumero = Math.floor ((Math.random ()*10)+1);
+ 	document.getElementById ('SegundoNumero').value = SegundoNumero;
+ 	Operador = ["*","+","/","-"] [Math.floor (Math.random()*4)];
+ 	document.getElementById('Operador').value = Operador;
+	temporizador = setInterval(Responder,4000);
+
+
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+	var CalculoCorrecto;
+	var Respuesta;
 
-
-}//FIN DE LA FUNCIÓN
+	if (Operador == "*")
+	{
+		CalculoCorrecto = (PrimerNumero*SegundoNumero);
+	}
+	if (Operador == "/")
+	{
+		CalculoCorrecto = (PrimerNumero/SegundoNumero);
+	}
+	if (Operador == "+")
+	{
+		CalculoCorrecto = (PrimerNumero+SegundoNumero);
+	}
+	if (Operador == "-")
+	{
+		CalculoCorrecto = (PrimerNumero-SegundoNumero);
+	}
+	CalculoCorrecto = parseInt (CalculoCorrecto);
+	console.log ("El resultado correcto es "+CalculoCorrecto);
+	Respuesta = document.getElementById('Respuesta').value;
+	if (CalculoCorrecto == Respuesta)
+	{
+		alert("El resultado es correcto");
+	}
+	else
+	{
+		alert("El resultado es incorrecto");
+	}
+	clearInterval (temporizador);
+}
