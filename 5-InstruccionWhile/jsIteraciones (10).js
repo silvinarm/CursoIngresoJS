fe39d21628e1da19f25943ab10bff23a7d91f5e8
silvinarm <silvinarm@hotmail.com>
2016-12-06 11:21:59 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,50 +1,59 @@
 function Mostrar()
 {
-
-	var contador=0;
-	var respuesta="si";
+	var respuesta;
 	var numeroIngresado;
-	var cantidadPositivos =0;
-	var	cantidadNegativos =0;
-	var cantidadCeros =0;
-	var sumaNegativos =0;
-	var sumaPositivos =0;
-	var cantidadPares =0;
-	var promedioPositivos =0;
-	var promedioNegativos =0;
+	var sumaNegativos;
+	var sumaPositivos;
+	var cantidadPositivos;
+	var cantidadNegativos;
+	var cantidadCeros;
+	var cantidadPares;
+	var promedioPositivos;
+	var promedioNegativos;
 	var diferencia;
 
-	while(respuesta!="no")
+	respuesta = "si";
+	sumaNegativos = 0;
+	sumaPositivos = 0;
+	cantidadPositivos = 0;
+	cantidadNegativos = 0;
+	cantidadCeros = 0;
+	cantidadPares = 0;
+
+	while (respuesta != "no")
 	{
 		numeroIngresado = prompt ("Ingrese número ");
 		numeroIngresado = parseInt (numeroIngresado);
-		if (numeroIngresado < 0)
+		if (numeroIngresado == 0)
 		{
-			cantidadNegativos == (cantidadNegativos+1);
-			sumaNegativos == (sumaNegativos + numeroIngresado);
-			if 
+			cantidadCeros = (cantidadCeros+1);
+		}
+		else
+		{
+			if (numeroIngresado%2 == 0)
 			{
 				cantidadPares = (cantidadPares+1);
 			}
+			if (numeroIngresado<0)
+			{
+				cantidadNegativos = (cantidadNegativos+1);
+				sumaNegativos = (sumaNegativos + numeroIngresado);
+			}
+			if (numeroIngresado>0)
+			{
+				cantidadPositivos = (cantidadPositivos+1);
+				sumaPositivos = (sumaPositivos + numeroIngresado);
+			}
 		}
-		if (numeroIngresado > 0)
-		{
-			cantidadPositivos == (cantidadPositivos+1);
-			sumaPositivos == (sumaPositivos + numeroIngresado);
-		}
-		if (numeroIngresado = 0) 
-		{
-			cantidadCeros == (cantidadCeros+1);
-		}			
 
 		respuesta = prompt ("Ingrese 'no' para finalizar");
-		contador ++;
-	}
 
+	}
+	console.log (cantidadPares);
 	promedioNegativos = (sumaNegativos/cantidadNegativos);
 	promedioPositivos = (sumaPositivos/cantidadPositivos);
 	diferencia = (sumaPositivos+sumaNegativos);
-	document.write ("1 - Suma de los negativos: "+sumaNegativos+" 2 - Suma de los positivos: "+sumaPositivos+" 3- Cantidad de positivos: "+cantidadPositivos+" 4 - Cantidad de negativos: "+cantidadNegativos+" 5 - Cantidad de ceros: "+cantidadCeros+" 6 - Cantidad de numeros pares: "+cantidadPares+" 7 - Promedio de positivos: "+promedioPositivos+" 8- Promedio de negativos: "+promedioNegativos);
+	document.write ("1 - Suma de los negativos: "+sumaNegativos+"<br>2 - Suma de los positivos: "+sumaPositivos+"<br>3- Cantidad de positivos: "+cantidadPositivos+"<br>4 - Cantidad de negativos: "+cantidadNegativos+"<br>5 - Cantidad de ceros: "+cantidadCeros+"<br>6 - Cantidad de numeros pares: "+cantidadPares+"<br>7 - Promedio de positivos: "+promedioPositivos+"<br>8- Promedio de negativos: "+promedioNegativos);
 
 
 
